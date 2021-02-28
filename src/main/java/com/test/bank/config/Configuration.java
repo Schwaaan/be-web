350f1f2efb6d4ca5d080371b321e116711ae59e6
Schwaaan <jair.schwan9@gmail.com>
2021-02-28 05:27:13 -0300

    add config cors

---
+++ src/main/java/com/test/bank/config/Configuration.java
@@ -0,0 +1,16 @@
+package com.test.bank.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("/**")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+  }
+}
